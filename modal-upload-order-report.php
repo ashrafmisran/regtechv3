1dfe84f0963ac10de00937af5c04abd7b56bc5d9
ashrafmisran <ashrafmisran@gmail.com>
2018-04-13 12:15:56 +0800

    Create action files to upload order report

---
+++ modal-upload-order-report.php
@@ -0,0 +1,67 @@
+<div class="modal fade" id="modal-upload-order-report">
+	<div class="modal-dialog modal-lg" role="document">
+		<form action="action-upload-order-report.php" method="post" class="modal-content" enctype="multipart/form-data">
+			<div class="modal-header">
+				<h4 class="modal-title">Upload AMLA008 Report</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					<span class="sr-only">Close</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="row">
+					<div class="col-md-3">
+						<label>Report Date</label>
+					</div>
+					<div class="col-md-9">
+						<input class="singledatepicker form-control" type="text" name="email-date" required>
+					    <small class="form-text text-muted mb-2">The date when the report is generated.</small>
+					</div>
+				</div>
+				<div class="row mb-1">
+					<div class="col-md-3">
+						<label>AMLA008 Report</label>
+					</div>
+					<div class="col-md-9">
+						<input type="file" name="report" class="form-control" accept=".pdf" required>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<label>Order ID</label>
+					</div>
+					<div class="col-md-9">
+						<select class="select2 form-control" multiple name="order-id[]">
+							<?php $run = $conn->query('SELECT * FROM order_48 WHERE status = "Replied"'); while ( $row = $run->fetch_assoc() ){ ?>
+								<option value="<?php echo $row['order_id'] ?>">ORD-<?php echo $row['order_id'] ?></option>
+							<?php } ?>
+						</select>
+					</div>
+				</div>
+				<div class="row mt-1">
+					<div class="col-md-3">
+						<label>Remark</label>
+					</div>
+					<div class="col-md-9">
+						<textarea class="form-control" name="remark"></textarea>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+				<button type="submit" class="btn btn-primary">Upload</button>
+			</div>
+		</form><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<script type="text/javascript">
+	$('#modal-upload-replied-email').on('shown.bs.modal', function (event) {
+		var button 	  = $(event.relatedTarget) // Button that triggered the modal
+		var id 	 	  = button.data('id')
+		var emailDate = button.data('email-date')
+		var modal 	  = $(this)
+		modal.find('#input-order-id').val(id)
+		modal.find('#input-email-date').val(emailDate)
+	})
+</script>
--- order-48.php
+++ order-48.php
@@ -47,10 +47,43 @@
 	  
 	  <!-- Report Tab Panel -->
 	  <div class="tab-pane fade" id="nav-report" role="tabpanel" aria-labelledby="nav-report-tab">
-
+	  	<button class="btn btn-primary my-1" data-toggle="modal" data-target="#modal-upload-order-report">Upload AMLA008 Report</button>
+	  	<div class="row">
+		  	<?php $run = $conn->query('SELECT * FROM order_48_report'); while ( $row = $run->fetch_assoc() ){ ?>
+		  		<div class="col-md-6">
+	  				<div class="card mb-4">
+	  					<div class="card-block m-2">
+	  						<h4 class="card-title">ORD-<?php echo $row['order_id'] ?></h4>
+	  						<p class="card-text">Order by <span class="badge badge-secondary"><?php echo($row['orderer']) ?></span> on <span class="badge badge-secondary"><?php echo($row['order_date']) ?></span>.<br>Email received on <span class="badge badge-secondary"><?php echo($row['receive_date']) ?></span>.</p>
+	  						<div class="row mb-2">
+	  							<div class="col-3">
+	  								<label>Files</label>
+	  							</div>
+	  							<div class="col">
+	  								<select class="form-control" onchange="window.open(this.value,'_blank')">
+	  									<option selected disabled>Choose to view...</option>
+	  									<?php 
+	  										$folder = 'docs/amla/' .substr($row['receive_date'],0,4). '/' .substr($row['receive_date'],5,2). '/' .substr($row['receive_date'],8,2). '/' .$row['order_id']. '/';
+	  										$files = scandir($folder); foreach($files as $file){ if($file == '..' OR $file =='.'){ continue; } ?>
+	  											<option value="<?php echo $folder.$file ?>"><?php echo $file; ?></option>
+	  									<?php } ?>
+	  								</select>
+	  							</div>
+	  						</div>
+	  						<a class="btn mb-1 btn-primary" href="generate-reply-email.php?to=<?php echo $row['reply_to'] ?>&cc=<?php echo $row['reply_cc'] ?>&subject=<?php echo $row['email_subject'] ?>&emaildate=<?php echo $row['receive_date'] ?>" target="_blank">Generate Email</a>
+	  						<button class="btn mb-1 btn-primary" data-toggle="modal" data-target="#modal-upload-replied-email" data-id="<?php echo $row['order_id']; ?>" data-email-date="<?php echo $row['receive_date']; ?>">Upload replied email</button>
+	  						<button class="btn mb-1 btn-danger"><i class="fas fa-trash-alt"></i></button>
+	  						<button class="btn mb-1 btn-danger">Confirm delete</button>
+	  						<button class="btn mb-1 btn-primary">Cancel</button>
+	  					</div>
+	  				</div>
+	  			</div>
+		  	<?php } ?>
+	  	</div>
 	  </div>
 	</div>
 
 </div>
 <?php include 'modal-add-order.php'; ?>
 <?php include 'modal-upload-replied-email.php'; ?>
+<?php include 'modal-upload-order-report.php'; ?>
