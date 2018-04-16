b6dafca5dc88845c9f9d8e1dd4fedb2ba95fa66f
ashrafmisran <ashrafmisran@gmail.com>
2018-04-16 12:58:37 +0800

    Add upload order report action file

---
+++ action-upload-order-report.php
@@ -0,0 +1,14 @@
+<?php
+	var_dump($_FILES);
+	var_dump($_POST);
+	
+	// Set the variable
+	$email_date = $_POST['email-date'];
+	$status		= 'Uploaded';
+	$remark		= $_POST['remark'];
+	// Upload file
+
+	// Insert into order_48_report table
+
+
+?>
